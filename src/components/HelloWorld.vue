54623ce77d2fdc8c543a5888a47003fa3d89e5dc
Rafid Adnan <princerafid01@gmail.com>
2019-10-29 23:01:03 +0600

    Added Algolia Auto complete location system. REady for deploy now.!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,8 @@
 <template>
     <div class="container">
-        <input ref="autocomplete"
-        placeholder="Search"
-        class="search-location"
-        onfocus="value = ''"
-        type="text" />
-
+        <input type="search" id="address-input" placeholder="Where are we going?" class="search-location"
+        onfocus="value = ''" ref="autocomplete"/>
+        <button class="btn btn-info" @click.prevent="showDi" v-if="showButton">{{ text }}</button>
         <div id="hello"></div>
     </div>
 </template>
@@ -13,6 +10,8 @@
 <script>
 import shp from "shpjs";
 import L from "leaflet";
+import jQuery from "jquery";
+import places from "places.js";
 // import google from "google-maps";
 import data from "./data";
 import slum from "./slum.json";
@@ -23,6 +22,9 @@ export default {
     data: function() {
         return {
             s: "",
+            text:'Show Direction',
+            showButton: false,
+            map:'',
             selected_lat: '',
             selected_lng: '',
             mapData: {
@@ -81,67 +83,99 @@ export default {
     props: {
         msg: String
     },
+    methods:{
+        showDi(){
+            if(this.text == 'Show Direction'){
+                this.text = 'Hide Direction';
+            } else {
+                this.text = 'Show Direction';
+            }
+            let links = document.querySelectorAll('.leaflet-right');
+            if(links){
+                [].forEach.call(links, (item) => {
+                    item.classList.toggle('none');
+                });
+            }
+        }
+    },
     mounted() {
         this.$nextTick(function() {
-            // var mymap = L.map("hello").setView([23.777176, 90.399452], 11); // initialize map
-            // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            //     attribution:
-            //         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            // }).addTo(mymap); // add tile
-            // L.Routing.control({
-            // waypoints: [
-            //     L.latLng(90.4103136062622, 23.752667863831423),
-            //     L.latLng(90.40932655334473, 23.754592574620776)
-            // ]
-            // }).addTo(mymap);
+            // Google api autocomplete
+            // this.autocomplete = new google.maps.places.Autocomplete(
+            //     (this.$refs.autocomplete), {types: ['geocode']}
+            // );
+            // this.autocomplete.addListener('place_changed', () => {
+            // let place = this.autocomplete.getPlace();
+            // let ac = place.address_components;
+            // this.selected_lat = place.geometry.location.lat();
+            // this.selected_lng = place.geometry.location.lng();
+            // let city = ac[0]["short_name"];
 
-            // var tear = new L.Icon({
-            //     iconUrl: "../logo.png"
+            // console.log(`The user picked ${city} with the coordinates ${lat}, ${lon}`);
             // });
-            // L.marker([23.777176, 	90.399452]).addTo(mymap).bindPopup('A pretty CSS3 popup.<br> Easily customizable.'); //add marker
-            // L.geoJSON(data.responseJSON, {
-            //   onEachFeature: function(feature, layer){
-            //     layer.bindPopup('Slum');
-            //     layer.setIcon(tear);
-            //   }
-            // }).addTo(mymap);
-            // L.geoJSON(this.mapData, {
-            //     style: function(feature) {
-            //         return {
-            //             color: "#000"
-            //         };
-			// 	}
-            // }).addTo(mymap);
-            this.autocomplete = new google.maps.places.Autocomplete(
-                (this.$refs.autocomplete), {types: ['geocode']}
-            );
-            this.autocomplete.addListener('place_changed', () => {
-            let place = this.autocomplete.getPlace();
-            let ac = place.address_components;
-            this.selected_lat = place.geometry.location.lat();
-            this.selected_lng = place.geometry.location.lng();
-            let city = ac[0]["short_name"];
+            // Google api ends for auto completing
 
-            console.log(`The user picked ${city} with the coordinates ${lat}, ${lon}`);
+            // Algolia api for autocompleting
+            let placesAutocomplete = places({
+                appId: 'plBUJSDMOSW1',
+                apiKey: '29b31d34608d6f4449699fc4aa6ce2bf',
+                container: document.querySelector('#address-input')
             });
-            navigator.geolocation.getCurrentPosition(function(location) {
+            placesAutocomplete.configure({
+                countries: ['bd'] // only search in the Bangladesh, the rest of the settings are unchanged: we're still searching for cities in German.
+            })
+            placesAutocomplete.on('clear', e => {
+                if(this.map) {
+                    this.map.remove();
+                }
+                this.showButton = false;
+            });
+            placesAutocomplete.on('change', e => {
+
+                this.selected_lat = e.suggestion.latlng.lat;
+                this.selected_lng = e.suggestion.latlng.lng;
+                navigator.geolocation.getCurrentPosition((location) => {
                 let lat = location.coords.latitude;
                 let lng = location.coords.longitude;
 
-            var map = L.map('hello').setView([23.777176, 90.399452], 11);;
+                this.map = L.map('hello').setView([23.777176, 90.399452], 5);;
 
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}{r}.png', {
-                attribution: '© OpenStreetMap contributors'
-            }).addTo(map);
-
-            L.Routing.control({
-                waypoints: [
-                    L.latLng(lat,lng),
-                    L.latLng(this.selected_lat, this.selected_lng),
-                ],
-                routeWhileDragging: true
-            }).addTo(map);
+                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}{r}.png', {
+                    attribution: '© OpenStreetMap contributors'
+                }).addTo(this.map);
+                if(this.selected_lat){
+                    let links = document.querySelectorAll('.leaflet-right');
+                    if(links){
+                        [].forEach.call(links, (item) => {
+                        item.classList.add('none');
+                        });
+                        this.showButton = true;
+                    }
+                    L.Routing.control({
+                        waypoints: [
+                            L.latLng(lat,lng),
+                            L.latLng(this.selected_lat, this.selected_lng),
+                        ],
+                        lineOptions: {
+                            styles: [
+                                { color: 'black', opacity: 0.15, weight: 9 },
+                                { color: 'white', opacity: 0.8, weight: 6 },
+                                { color: 'steelblue', opacity: 1, weight: 4 }
+                            ]
+                        },
+                        altLineOptions: {
+                            styles: [
+                                { color: 'black', opacity: 0.15, weight: 9 },
+                                { color: 'white', opacity: 0.8, weight: 6 },
+                                { color: 'hotpink', opacity: 1, weight: 4 }
+                            ]
+                        }
+                    }).addTo(this.map);
+                }
+                });
             });
+
+
         });
     }
 };
@@ -168,16 +202,34 @@ a {
     height: 480px;
 }
 .search-location {
-  display: block;
-  width: 60vw;
-  margin: 0 auto;
-  margin-top: 5vw;
-  font-size: 20px;
-  font-weight: 400;
-  outline: none;
-  height: 30px;
-  line-height: 30px;
-  text-align: center;
-  border-radius: 10px;
+      display: block;
+    width: 60vw;
+    margin: 0 auto;
+    margin-top: 5vw;
+    font-size: 20px;
+    font-weight: 400;
+    outline: none;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    border-radius: 5px;
+    padding: 27px;
+    margin-bottom: 5vw;
+}
+.none {display: none;}
+button.btn.btn-info {
+    background-color:#44c767;
+	border-radius:8px;
+	border:1px solid #18ab29;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:16px 31px;
+	text-decoration:none;
+    margin: 0 auto;
+    display: block;
+    margin-bottom: 5vw;
 }
 </style>
